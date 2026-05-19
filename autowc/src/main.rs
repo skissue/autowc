7cc2f57ec188b6a693bc57934a2ebf118d412220
Ad <me@skissue.xyz>
2026-05-19 09:40:25 -0400

    refactor: move autowc into workspace

--- autowc/src/main.rs
+++ autowc/src/main.rs
@@ -75,9 +75,14 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 fn init_logging() {
     if let Ok(env_filter) = tracing_subscriber::EnvFilter::try_from_default_env() {
-        tracing_subscriber::fmt().with_env_filter(env_filter).init();
+        tracing_subscriber::fmt()
+            .with_writer(std::io::stderr)
+            .with_env_filter(env_filter)
+            .init();
     } else {
-        tracing_subscriber::fmt().init();
+        tracing_subscriber::fmt()
+            .with_writer(std::io::stderr)
+            .init();
     }
 }
 
